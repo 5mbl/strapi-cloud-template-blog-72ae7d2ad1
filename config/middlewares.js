185e09d33a2725f05ae5f042e11fe08aa488fc40
pizza-fish <nyrox.plg@gmail.com>
2024-08-08 15:19:45 +0200

    custom middleware ensure cors headers always sent

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,40 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
   {
-    name: 'strapi::cors',
+    name: "strapi::cors",
     config: {
-      origin: '*', // Accept requests from all origins
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      origin: "*", // Accept requests from all origins
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
       keepHeaderOnError: true,
     },
   },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  // Custom middleware to ensure CORS headers are set
+  {
+    name: "strapi::custom-cors",
+    config: {
+      beforeInitialize() {
+        strapi.server.use((ctx, next) => {
+          ctx.set("Access-Control-Allow-Origin", "*");
+          ctx.set(
+            "Access-Control-Allow-Methods",
+            "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
+          );
+          ctx.set(
+            "Access-Control-Allow-Headers",
+            "Content-Type, Authorization, Origin, Accept"
+          );
+          return next();
+        });
+      },
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
