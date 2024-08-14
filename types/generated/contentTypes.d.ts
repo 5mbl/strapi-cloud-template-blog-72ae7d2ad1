e9e1fa1c527fa1ae4af332b83df5e99f74317161
pizza-fish <nyrox.plg@gmail.com>
2024-08-14 20:39:48 +0200

    change

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -907,10 +907,10 @@ export interface ApiProjectArticleProjectArticle extends Schema.CollectionType {
     draftAndPublish: true;
   };
   attributes: {
-    Description: Attribute.String;
-    Title: Attribute.String;
-    Cover: Attribute.Media;
-    Main: Attribute.Blocks;
+    description: Attribute.String;
+    title: Attribute.String;
+    cover: Attribute.Media;
+    main: Attribute.Blocks;
     categories: Attribute.Relation<
       'api::project-article.project-article',
       'manyToMany',
@@ -921,9 +921,9 @@ export interface ApiProjectArticleProjectArticle extends Schema.CollectionType {
       'oneToOne',
       'api::author.author'
     >;
-    SEOtitel: Attribute.String;
-    SEOdesc: Attribute.Text;
-    Resources: Attribute.Blocks;
+    seotitel: Attribute.String;
+    seodesc: Attribute.Text;
+    resources: Attribute.Blocks;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
